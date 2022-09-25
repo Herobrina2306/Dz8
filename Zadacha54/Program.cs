e0e66469fd2ca6d74c46708b93fe4d8552a82d11
AleksandraM <musatova.aleksandra@mail.ru>
2022-09-25 13:44:30 +0300

    Сделаны задачи 58, 60 и 62

---
+++ Zadacha54/Program.cs
@@ -0,0 +1,99 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.Clear();
+
+Console.WriteLine("Введите количество строк");
+int row = int.Parse(Console.ReadLine() ?? string.Empty);
+
+Console.WriteLine("Введите количество столбцов");
+int columns = int.Parse(Console.ReadLine() ?? string.Empty);
+
+int[,] array = GetArray(row, columns, 0, 9);
+PrintArray(array);
+
+Console.WriteLine();
+
+int[,] newArray = SelectionSort(array);
+PrintArray(newArray);
+
+int[,] GetArray(int m, int n, int minVaiue, int maxVaiue)
+{
+    int[,] result = new int[m, n];
+    for(int i = 0; i<m; i++)
+    {
+        for(int j = 0; j<n; j++)
+        {
+            result[i,j] = new Random().Next(minVaiue, maxVaiue +1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for(int i = 0; i<inArray.GetLength(0); i++)
+    {
+        for(int j = 0; j<inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+// int[,] ArrayInDescendingOrder (int[,] arr)
+// {
+//     int r = arr.GetLength(0);
+//     int c = arr.GetLength(1); 
+//     for(int i = 0; i <r; i++)
+//     {
+        
+//         int k = arr[i,0];
+//         for (int j = 0; j<c; j++)
+//         {
+//             int minPosition = j;
+//             int m = 0;
+//             if(arr[i,j] > arr[i,minPosition])
+//             {
+//                 minPosition = j;
+//             }
+//             m = arr[i,j];
+//             arr[i,j] = arr[i,minPosition];
+//             arr[i,minPosition] = m;
+            
+//         }
+        
+//     }
+//     return arr;
+// }
+
+int[,] SelectionSort(int[,] array)
+{
+    int[,] arrayNew = new int[array.GetLength(0),array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            int minPosition = j;
+            int m = 0;
+
+            for (int k = j+1; k < array.GetLength(1) ; k++)
+            {
+                
+                if(array[i,k] < m) m = array[i,k];
+
+            }
+            arrayNew[i,minPosition] = m;
+        }
+    }
+    return arrayNew;
+}
---
+++ Zadacha58/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+Console.Clear();
+
+Console.WriteLine("Введите количество строк");
+int row = int.Parse(Console.ReadLine() ?? string.Empty);
+
+Console.WriteLine("Введите количество столбцов");
+int columns = int.Parse(Console.ReadLine() ?? string.Empty);
+
+int[,] array1 = GetArray(row, columns, 0, 9);
+PrintArray(array1);
+Console.WriteLine();
+
+int[,] array2 = GetArray(row, columns, 0, 9);
+PrintArray(array2);
+
+Console.WriteLine();
+
+int[,] newArray = Composition(array1, array2);
+PrintArray(newArray);
+
+int[,] GetArray(int m, int n, int minVaiue, int maxVaiue)
+{
+    int[,] result = new int[m, n];
+    for(int i = 0; i<m; i++)
+    {
+        for(int j = 0; j<n; j++)
+        {
+            result[i,j] = new Random().Next(minVaiue, maxVaiue +1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for(int i = 0; i<inArray.GetLength(0); i++)
+    {
+        for(int j = 0; j<inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Composition(int[,] arr1, int[,] arr2)
+{
+    int r = arr1.GetLength(0);
+    int c = arr1.GetLength(1);
+    int[,] arrNew = new int[r,c];
+    for(int i = 0; i<arrNew.GetLength(0); i++)
+    {
+        for(int j = 0; j<arrNew.GetLength(1); j++)
+        {
+            arrNew[i,j] = arr1[i,j] * arr2[i,j];
+        }
+    }
+    return arrNew;
+}
